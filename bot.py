990d0a81ac63c9ddddbaa3863b06d741509cff15
Alice Montel <alice.mntl@gmail.com>
2018-07-10 17:26:21 +0200

    adding bot class

---
+++ bot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 10 17:08:13 2018
+
+@author: alice.montel
+"""
+class bot:
+    def __init__(self):
+        self.name="Discobot"
+        
+
---
+++ discobot.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 10 17:07:23 2018
+
+@author: alice.montel
+"""
+from bot import bot
+
+if __name__=='__main__':
+    print("Here we go !")
+    mybot = bot()
+    print("I am "+mybot.name)
